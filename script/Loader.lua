f1de2706f75e5a6e780cc775a92424648f2d4506
WeLoveCandy <121447039+WeLoveCandy@users.noreply.github.com>
2023-01-02 23:37:53 +0700

    Update Loader.lua

--- script/Loader.lua
+++ script/Loader.lua
@@ -5253,10 +5253,10 @@ end
 
 -- // Window
 
-Date = os.date("%d".." ".."%B".." ".."%Y")
+
 local window =
     library:new(
-    {textsize = 13.5, font = Enum.Font.RobotoMono, name = "Krahub"..date, color = Color3.fromRGB(255, 64, 64)}
+    {textsize = 13.5, font = Enum.Font.RobotoMono, name = "Krahub" color = Color3.fromRGB(255, 64, 64)}
 )
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/1201for/V.G_Hub_Extras/main/Universal_Client_Bypass"))()
@@ -5287,3 +5287,13 @@ local MainR = Main:section({name = "Teleport", side = "right", size = 60})
 local MiscL = Visual:section({name = "Player", side = "left", size = 75})
 
 local SettingL = Setting:section({name = "Settings", side = "left", size = 60})
+
+-- script --
+Main:button(
+    {
+        name = "Fast farm",
+        callback = function()
+            print("Opened")
+        end
+    }
+)
